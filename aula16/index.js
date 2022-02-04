6440520132470cedd0599602e77d7350465e474e
clefersondannylo <clefersonnascimento8@gmail.com>
2022-02-03 22:28:24 -0300

    Arrays-básico

---
+++ aula16/index.js
@@ -0,0 +1,8 @@
+let nomes = ["Jó", "Maria", "Isaque", "Urias"];
+let removido = nomes.pop();
+// console.log(removido);
+// console.log(nomes);
+
+nomes[nomes.length] = "Edina";
+console.log(nomes);
+console.log(nomes instanceof Array);
