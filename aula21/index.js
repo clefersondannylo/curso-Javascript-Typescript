2979b683b84ae21cac6fa73b275287e568233a54
clefersondannylo <clefersonnascimento8@gmail.com>
2022-02-08 17:37:51 -0300

    aula operadores de comparação

---
+++ aula21/index.js
@@ -0,0 +1,29 @@
+//Operadores Lógicos de Comparação
+/*
+
+> Maior que
+>= Maior que ou igual a
+< Menor igual
+<=Menor que ou igual a
+== igualdade
+=== igualdade estrita
+!= desigualdade(diferente)
+!== desigualdade estrita
+
+*/
+
+console.log(10 > 5); //true
+
+console.log(5 >= 5); //true
+
+console.log(10 < 5); //false
+
+console.log(6 <= 5); //false
+
+console.log(10 == "10"); //true
+
+console.log(10 === "10"); //false
+
+console.log(5 != "5"); //false
+
+console.log(5 !== "5"); //true
